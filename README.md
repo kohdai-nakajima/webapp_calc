c95533c2df2db78f9480eb29e15a3830f3a2b0d6
A <a@enoMacBook-puro.local>
2023-10-23 08:56:52 +0900

    リロード機能を追加

--- README.md
+++ README.md
@@ -24,6 +24,9 @@ WebARENAのインスタンスがちょいちょい停止になるので、
 - [こちら](https://qiita.com/MotohiroSiobara/items/b672b22ce0505e5e17de)を参考にディレクトリ構成を作成
 - 処理フローは以下 (処理内容は[こちら](https://snowsystem.net/javascript/typescript/express-typescript-rest-api/#)も参考)
 - controllerとrouteの使い分けは[こちら](https://reffect.co.jp/node-js/first-time-express-js/#controllers-%E3%81%AE%E4%BD%9C%E6%88%90)を参照
+  - /public/  -> Expressの静的ファイル置き場([こちら](https://expressjs.com/ja/starter/static-files.html)を参照)
+    - index.html  -> ReactAppのbuild結果
+    - static/  -> ReactAppのbuild結果(cssやjsを格納)
   - /src/api/
     - server.ts  -> main処理、"/math"をmath/index.tsに振り分ける
     - route/
@@ -39,17 +42,31 @@ WebARENAのインスタンスがちょいちょい停止になるので、
   - CORSについては[こちら](https://qiita.com/10mi8o/items/2221134f9001d8d107d6)を参照
   - 解決策については[こちら](https://qiita.com/tomoya_ozawa/items/feca4ffc6217d585b037)を参照
 
-# サーバの環境構築メモ
-- Node.jsをサーバにインストール
-- 作ったやつをサーバにアップ
-- npm install
-- npm run server
+# サーバデプロイ方法
+サーバでfrontのデプロイをするとサーバが死ぬので、ローカルでビルドしてgithub経由で渡す
+サーバデプロイ周り、他のチームがどうやってるのか気になってはいる、ちゃんとやるならhtaccessとかいじる必要ありそう(詳細は[こちら](https://qiita.com/neras_1215/items/27c16c605a367ebe88d9))
+- front/src/App.tsxの以下をコメントアウトする (なんか自動化できればいいな)
+  - apiServerUrlの変数定義
+  - apiServerUrlを利用している個所 (相対パスにする)
+- ローカルで以下のコマンドを叩く
+  - cd webapp_calc/front
+  - npm run build
+- webapp_calc/front/build配下の以下のファイルをwebapp_calc/back/public配下にcpする
+  - index.html
+  - static/配下全て
+- gitにpushする
+- serverに入ってgit pull
+- webapp_calc/back配下で以下コマンドを実行
+  - npm run server &
+  - 「port:8080で起動中」と表示されたらControl＋Cで中断する
 
-# ローカルの環境構築メモ
+# 環境構築メモ
 基本的な手順は[こちら](https://ralacode.com/blog/post/create-nodejs-react-app-with-typescript/)の通り
 以下、MacOS MacOS High Sierra(10.13.6) で詰まったところを記載
 - nodeの最新版を入れるとnode -vでエラーになるので、v15.14.0を入れた
   - [参考](https://chaika.hatenablog.com/entry/2018/06/07/090000)
 - npm init -yを叩くとnpmのバージョンを上げろと言われるが、nodeのバージョンと会っていないとあげられないので、あげない
   - [Nodeとnpmのバージョン対比表](https://nodejs.org/ja/download/releases)
-- 
+
+# Reactの諸々
+- [React hooks周り](https://qiita.com/seira/items/e62890f11e91f6b9653f)
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 // const apiServerUrl: string = "http://localhost:8080";
+// const calculaterGameEndpoint: string = apiServerUrl + "/api/math/calculater-game";
 const calculaterGameEndpoint: string = "/api/math/calculater-game";
 
 type CalculaterGameData = {
@@ -8,6 +9,11 @@ type CalculaterGameData = {
   answer: string;
 };
 
+/**
+ * シンプルに答えを表示するだけの処理.
+ * 
+ * @param event clickイベント.
+ */
 const displayAnswer = (event: React.MouseEvent<HTMLButtonElement>): void => {
   const answer: HTMLInputElement = document.querySelectorAll('.answer')[0] as HTMLInputElement;
   answer.style.display = 'block';
@@ -16,21 +22,39 @@ const displayAnswer = (event: React.MouseEvent<HTMLButtonElement>): void => {
   button.style.display = 'none';
 }
 
+/**
+ * シンプルに答えを隠すだけの処理.
+ */
+const hideAnswer = (): void => {
+  const answer: HTMLInputElement = document.querySelectorAll('.answer')[0] as HTMLInputElement;
+  answer.style.display = 'none';
+
+  const button: HTMLInputElement = document.querySelectorAll('.button')[0] as HTMLInputElement;
+  button.style.display = 'initial';
+}
+
 const App: React.FC = () => {
   const [calculaterGameData, setCalculaterGameData] = React.useState<CalculaterGameData | null>(null);
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // const res = await fetch(apiServerUrl + calculaterGameEndpoint);
-        const res = await fetch(calculaterGameEndpoint);
-        const json: React.SetStateAction<CalculaterGameData | null> = await res.json();
-        setCalculaterGameData(json);
-      } catch (e: unknown) {
-        console.error(e);
-      }
-    };
+  /**
+   * 計算ゲーム用情報取得APIを叩いて、取得したデータでstate(calculaterGameData)を更新する.
+   */
+  const fetchData = async(): Promise<void> => {
+    try {
+      const res = await fetch(calculaterGameEndpoint);
+      // const res = await fetch(calculaterGameEndpoint);
+      const json: React.SetStateAction<CalculaterGameData | null> = await res.json();
+      setCalculaterGameData(json);
+    } catch (e: unknown) {
+      console.error(e);
+    } finally {
+      // 答えを隠す
+      hideAnswer();
+    }
+  }
 
+  // useEffect：：関数の実行タイミングをReactのレンダリング後まで遅らせるHook.
+  React.useEffect(() => {
     fetchData();
   }, []);
 
@@ -42,6 +66,7 @@ const App: React.FC = () => {
       <button className="button" onClick={displayAnswer}>こたえを見る</button>
       <div className="answer">
         <p>こたえ：{calculaterGameData?.answer}</p>
+        <button className="button" onClick={fetchData}>つぎのもんだい</button>
       </div>
     </div>
   );
